c34d80305fabb282e088c874aea429bcefa75c6d
michielvissenberg <michiel@vissenberg.be>
2026-02-26 19:13:03 +0100

    small change

--- pdf_parser/main.py
+++ pdf_parser/main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-# Import our modular parts
 from models.schemas import FinancialLineItem, FinancialStatement
 from processors.cleaners import clean_financial_value
 
